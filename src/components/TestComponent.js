d467ee5906b163d421b75202c1431204605eca9a
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-12 15:39:35 +0530

    Added Functional Component

---
+++ src/components/TestComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestComponent = () => {
+    return (
+        <div>
+            <h1>Hello from Component!</h1>
+            <p>This is a functional component in React.</p>
+        </div>
+    );
+};
+
+export default TestComponent;
