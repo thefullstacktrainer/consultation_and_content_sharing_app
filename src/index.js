dbc86d2db68c23bf7128b38fbe588e4bf09124e5
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-17 03:23:59 +0530

    redux incomplete code

--- src/index.js
+++ src/index.js
@@ -13,11 +13,14 @@ import reportWebVitals from './reportWebVitals';
 // import ClassComponent from './components/ClassComponent'; // Adjust the import path as needed
 
 // ReactDOM.render(<ClassComponent />, document.getElementById('root'));
-
+import store from './store';
+import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
