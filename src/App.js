a90132d4b0d641574445bd5295fa6bba61e8850f
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-16 15:06:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import ClassComponent from './components/ClassComponent';
+import TestComponent from './components/TestComponent';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ClassComponent />
+      <TestComponent />
+
     </div>
   );
 }
