2894679b70008f50154d06d4f16dc6fcfdfa301a
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-22 11:03:09 +0530

    job portal

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function JobList({ jobPosts }) {
+    const [applicationStatus, setApplicationStatus] = useState({});
+    const [selectedJob, setSelectedJob] = useState(null);
+
+    const handleApply = (jobId) => {
+        // Simulate applying for a job
+        alert(`You applied for the job with ID ${jobId}`);
+    };
+
+    const handleStatusChange = (jobId, applicationId, newStatus) => {
+        setApplicationStatus(prevStatus => ({
+            ...prevStatus,
+            [`${jobId}-${applicationId}`]: newStatus,
+        }));
+    };
+
+    const handleJobSelect = (jobId) => {
+        setSelectedJob(jobId === selectedJob ? null : jobId);
+    };
+
     return (
         <div className="job-list">
             <h2>Job Posts</h2>
@@ -8,13 +27,19 @@ function JobList({ jobPosts }) {
                 <div key={jobPost.id} className="job-item">
                     <h3>{jobPost.title}</h3>
                     <p>{jobPost.company} - {jobPost.location}</p>
-                    <p>Type: {jobPost.type}</p>
-                    <p>{jobPost.description}</p>
+                    {/* ... other job details ... */}
+                    <button onClick={() => handleApply(jobPost.id)}>Apply</button>
                     <h4>Recent Applications:</h4>
                     {jobPost.applications.map(application => (
                         <div key={application.id} className="application">
                             <p>{application.candidateName}</p>
-                            <p>Status: {application.status}</p>
+                            <p>Status: {applicationStatus[`${jobPost.id}-${application.id}`] || application.status}</p>
+                            <button onClick={() => handleStatusChange(jobPost.id, application.id, 'Pending')}>
+                                Mark as Pending
+                            </button>
+                            <button onClick={() => handleStatusChange(jobPost.id, application.id, 'Reviewed')}>
+                                Mark as Reviewed
+                            </button>
                         </div>
                     ))}
                     {jobPost.relatedJob && (
@@ -24,6 +49,14 @@ function JobList({ jobPosts }) {
                             <p>{jobPost.relatedJob.company} - {jobPost.relatedJob.location}</p>
                             <p>Type: {jobPost.relatedJob.type}</p>
                             <p>{jobPost.relatedJob.description}</p>
+                            <button onClick={() => handleJobSelect(jobPost.relatedJob.id)}>
+                                {selectedJob === jobPost.relatedJob.id ? 'Hide Details' : 'View Details'}
+                            </button>
+                            {selectedJob === jobPost.relatedJob.id && (
+                                <div className="job-details">
+                                    {/* Display detailed information about the recommended job */}
+                                </div>
+                            )}
                         </div>
                     )}
                 </div>
--- src/components/OverviewBanner.js
+++ src/components/OverviewBanner.js
@@ -1,6 +1,9 @@
 import React from 'react';
-
+import { useState } from 'react'
 function OverviewBanner() {
+    const [banner, updateBanner] = useState("")
+
+
     return (
         <div className="overview-banner">
             <h1>Welcome to Our Job Board</h1>
