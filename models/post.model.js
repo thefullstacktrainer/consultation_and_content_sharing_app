2ab198903588a4466c7291e16ea6dca10f8d211a
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-31 18:24:18 +0530

    changes

--- models/post.model.js
+++ models/post.model.js
@@ -18,18 +18,18 @@ function getPosts() {
 function getPost(id) {
     return new Promise((resolve, reject) => {
         helper.mustBeInArray(posts, id)
-        .then(post => resolve(post))
-        .catch(err => reject(err))
+            .then(post => resolve(post))
+            .catch(err => reject(err))
     })
 }
 
 function insertPost(newPost) {
     return new Promise((resolve, reject) => {
         const id = { id: helper.getNewId(posts) }
-        const date = { 
+        const date = {
             createdAt: helper.newDate(),
             updatedAt: helper.newDate()
-        } 
+        }
         newPost = { ...id, ...date, ...newPost }
         posts.push(newPost)
         helper.writeJSONFile(filename, posts)
@@ -40,37 +40,37 @@ function insertPost(newPost) {
 function updatePost(id, newPost) {
     return new Promise((resolve, reject) => {
         helper.mustBeInArray(posts, id)
-        .then(post => {
-            const index = posts.findIndex(p => p.id == post.id)
-            id = { id: post.id }
-            const date = {
-                createdAt: post.createdAt,
-                updatedAt: helper.newDate()
-            } 
-            posts[index] = { ...id, ...date, ...newPost }
-            helper.writeJSONFile(filename, posts)
-            resolve(posts[index])
-        })
-        .catch(err => reject(err))
+            .then(post => {
+                const index = posts.findIndex(p => p.id == post.id)
+                id = { id: post.id }
+                const date = {
+                    createdAt: post.createdAt,
+                    updatedAt: helper.newDate()
+                }
+                posts[index] = { ...id, ...date, ...newPost }
+                helper.writeJSONFile(filename, posts)
+                resolve(posts[index])
+            })
+            .catch(err => reject(err))
     })
 }
 
 function deletePost(id) {
     return new Promise((resolve, reject) => {
         helper.mustBeInArray(posts, id)
-        .then(() => {
-            posts = posts.filter(p => p.id !== id)
-            helper.writeJSONFile(filename, posts)
-            resolve()
-        })
-        .catch(err => reject(err))
+            .then(() => {
+                posts = posts.filter(p => p.id != id)
+                helper.writeJSONFile(filename, posts)
+                resolve()
+            })
+            .catch(err => reject(err))
     })
 }
 
 module.exports = {
     insertPost,
     getPosts,
-    getPost, 
+    getPost,
     updatePost,
     deletePost
 }
