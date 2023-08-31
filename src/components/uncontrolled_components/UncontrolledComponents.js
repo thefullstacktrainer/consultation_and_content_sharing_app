c42d0f9d3e0d0541b330c08918a3ff33b1b631ea
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-08-31 17:20:30 +0530

    changes

--- src/components/uncontrolled_components/UncontrolledComponents.js
+++ src/components/uncontrolled_components/UncontrolledComponents.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CheckboxGroup from './CheckboxGroup'
+import MultiSelectDropdown from './MultiSelectDropdown'
 
 function UncontrolledComponents() {
     return (
         <>
             <div>UncontrolledComponents</div>
             <CheckboxGroup />
+            <MultiSelectDropdown />
         </>
     )
 }
