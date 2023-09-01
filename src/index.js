46b354f6625f4bc961654d6f65b95b78e6a9abef
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-09-01 17:04:31 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
+import Main from './consultation/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    {/* <App /> */}
+    <Main />
   </Router>
 );
 
