307ec01e3fa5966b71ec1499284c7cb665df5029
LAKSHMIKANT <lakshmikant4u@gmail.com>
2023-09-01 17:36:30 +0530

    changes

--- src/consultation/Main.js
+++ src/consultation/Main.js
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { Routes, Route, Link } from 'react-router-dom';
 function Main() {
+    const users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
-        <div>Main</div>
-    )
+        <div className="">
+            <div className="mx-8 flex gap-6 my-8">
+                {
+                    users.map((user, index) => {
+                        return (
+                            <Link key={index} to={`users/${user}`}>User {user}</Link>
+                        )
+                    })
+                }
+            </div>
+            <Routes>
+                <Route path="users/:userId" element={<div>Main</div>} />
+            </Routes>
+
+        </div>
+    );
 }
 
 export default Main
